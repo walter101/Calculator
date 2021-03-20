bbc3884869d0125211b433112d52ab4a27632d02
Walter Pothof <walterpothof@gmail.com>
2021-03-20 14:40:40 +0100

    my fist commit

---
+++ src/Calculator.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+class Calculator
+{
+    /**
+     * @param $value
+     *
+     * @return float|int
+     */
+    public function wp_double($value)
+    {
+        return $value * 2;
+    }
+}
